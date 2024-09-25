e134b2dd50ede429ec05bcae1a1fb84ea494e335
Panda <pickledchimeraarmy@gmail.com>
2024-09-26 00:02:43 +1000

    e2

--- app.py
+++ app.py
@@ -3,54 +3,78 @@ import requests
 from streamlit_lottie import st_lottie
 from PIL import Image
 
-st.set_page_config(page_title="FreezingPanda's Web Portfolio", page_icon="", layout="wide")
+# Set page configuration
+st.set_page_config(page_title="FreezingPandaDev", page_icon=":panda_face:", layout="wide")
 
+# Load Lottie animation from URL
 def load_lottieurl(url):
- r = requests.get(url)
- if r.status_code != 200:
-    st.write("No animation rn :()")
-    return None
-    
- return r.json()
+    r = requests.get(url)
+    if r.status_code != 200:
+        st.write("No animation rn :()")
+        return None
+    return r.json()
 
-st.subheader("Hello, I am Panda")
-st.title("An Australian based high school student, gamedev & programmer")
-
-st.write("I am passionate about game development and app creation, I've always wanted to work with code and make software. And here I am now!")         
-
-lottie_coding = load_lottieurl("https://lottie.host/ff849127-3b12-4969-8108-77fb056224c1/V0AFiFDmxq.json")
+# Use local images for portfolio
 img_gm = Image.open("images/gm.png")
 img_contact_form_vs = Image.open("images/vs.png")
+
+# Lottie animation link
+lottie_coding = load_lottieurl("https://lottie.host/ff849127-3b12-4969-8108-77fb056224c1/V0AFiFDmxq.json")
+
+# Hero Section (Introduction)
 with st.container():
- left_column, right_column = st.columns(2)
- with left_column:
-  st.write("##")
-  st.write("""Ever since I first played a game on the Xbox 360, I wondered how it all worked, what made it 
-          tick and how you added more to it, I never really looked too far into it until 2023, when I downloaded
-           Python and VS Code for the first time, and wrote my first program""")
-  st.write("I plan to eventually make my dream game when I develop more programming skills, right now it's just a lot of learning and getting better!")
-  
- st.write("---")
- st.subheader("Links:")
+    st.title("👋 Hello, I'm Panda")
+    st.write("### An Australian-based high school student, game developer, and programmer.")
+    st.write(
+        "I'm passionate about creating games and apps. Ever since I started learning Python and exploring the world of development, "
+        "I've been fascinated by the endless possibilities of what I can create."
+    )
 
- st.write("[GameJolt]()")
- st.write("[GitHub](https://github.com/FreezingPanda)")
- st.write("[lastfm](https://www.last.fm/user/FreezingPanda)")
- st.write("[guns.lol](https://guns.lol/freezingpanda)")
+# Animation next to Introduction
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.write("##")
+        st.write(
+            """
+            Since playing my first game on the Xbox 360, I’ve always wondered how they worked. 
+            In 2023, I downloaded Python and VS Code for the first time, and wrote my first program. 
+            My goal is to continue learning and eventually create my dream game!
+            """
+        )
+    with right_column:
+        st_lottie(lottie_coding, height=300, key="coding")
 
-with right_column:
-  st_lottie(lottie_coding, height=300, key="coding")
+# Divider
+st.write("---")
+
+# Links Section
+st.subheader("🔗 Connect with me")
+st.write("You can find me on these platforms:")
+columns = st.columns(4)
+columns[0].subheader("[GameJolt]()")
+columns[1].subheader("[GitHub](https://github.com/FreezingPanda)")
+columns[2].subheader("[lastfm](https://www.last.fm/user/FreezingPanda)")
+columns[3].subheader("[guns.lol](https://guns.lol/freezingpanda)")
+
+# What I Use Section
+st.write("---")
+st.header("🛠 What I Use")
+st.write("### Tools & Software")
+tools_column, description_column = st.columns((1, 2))
+with tools_column:
+    st.image(img_contact_form_vs, caption="Visual Studio Code")
+    st.image(img_gm, caption="GameMaker")
+with description_column:
+    st.write(
+        """
+        For app development and programming, I primarily use [Visual Studio Code](https://code.visualstudio.com/) and [Python](https://www.python.org/). 
+        For game development, I use [GameMaker](https://gamemaker.io/en), which allows me to bring my creative ideas to life.
+        """
+    )
+
+# Resume Download Section
 
 
-with st.container():
-  st.write("---")
-  st.header("What I use")
-  st.header("##")
-  image_column, text_column = st.columns((1, 2))
-  with image_column:
-    st.image(img_contact_form_vs)
-    st.image(img_gm)
-  with text_column:
-    st.subheader("The software I use")
-    st.write("I [use Visual Studio Code](https://code.visualstudio.com/) and [Python](https://www.python.org/) for standard programming and app development, but for game development I use [GameMaker](https://gamemaker.io/en)")
-
+# Footer for final section
+st.write("---")
+st.write("### Made with ❤️ by Panda")
